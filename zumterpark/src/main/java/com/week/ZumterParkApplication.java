9e7572a3f0cfd80129f0e31d128de9eaf06d8427
minsoozz <minsoo1604@naver.com>
2023-03-27 02:00:52 +0900

    chore: 프로젝트 초기 설정, 과제별 멀티 모듈 구성

--- zumterpark/src/main/java/com/week/ZumterParkApplication.java
+++ zumterpark/src/main/java/com/week/ZumterParkApplication.java
@@ -1,13 +1,12 @@
-package com.week.zumgnmarket;
+package com.week;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZumgnmarketApplication {
+public class ZumterParkApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZumgnmarketApplication.class, args);
+        SpringApplication.run(ZumterParkApplication.class);
     }
-
 }
